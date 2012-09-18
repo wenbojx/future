320ebe762ddeab865a3500bcc0f558ec3d6e3537
李文博 <wbli@anjuke.com>
2012-09-18 16:27:00 +0800

    a

--- protected/config/main.php
+++ protected/config/main.php
@@ -27,8 +27,8 @@ return array(
         // uncomment the following to enable the Gii tool
 
         'gii'=>array(
-            'class'=>'system.gii.GiiModule',
-            'password'=>'111',
+            //'class'=>'system.gii.GiiModule',
+            //'password'=>'222',
              // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters'=>array('127.0.0.1','::1'),
         ),
