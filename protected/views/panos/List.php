a852fce988cf3ddefd8344e2c90ff3a4a579ec1e
李文博 <wbli@anjuke.com>
2012-09-18 16:55:06 +0800

    1

--- protected/views/panos/List.php
+++ protected/views/panos/List.php
@@ -1,10 +1,10 @@
 <div>
-<?php if($datas['list']){ foreach($datas['list'] as $v){?>
-	<dl>
-		<dt>
-			<?php echo CHtml::link($v['name'],array('panos/pano/show/','id'=>$v['id']));?>
-		</dt>
-		<dd></dd>
-	</dl>
+<?php if(isset($datas['list'])){ foreach($datas['list'] as $v){?>
+    <dl>
+        <dt>
+            <?php echo CHtml::link($v['name'],array('panos/pano/show/','id'=>$v['id']));?>
+        </dt>
+        <dd></dd>
+    </dl>
 <?php }}?>
 </div>
