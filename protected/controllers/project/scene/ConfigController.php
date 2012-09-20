b60a426de234227232aa227377b3d5d3cc6b188f
李文博 <wbli@anjuke.com>
2012-09-20 17:52:04 +0800

    1

---
+++ protected/controllers/project/scene/ConfigController.php
@@ -0,0 +1,46 @@
+<?php
+class ConfigController extends Controller{
+    public $defaultAction = 'v';
+    public $defaultType = array(
+            'position', 'basic', 'preview', 'view', 'hotspot',
+            'button', 'map', 'navigat', 'radar',
+            'html', 'rightkey', 'link', 'flare','action','thumb'
+            );
+
+    public function actionV(){
+        $request = Yii::app()->request;
+        $datas = array();
+        $scene_id = $request->getParam('scene_id');
+        $datas['scene_id'] = $scene_id;
+        $type = $request->getParam('t');
+        if ($type == 'hotspot'){
+            $datas['link_scene'] = $this->get_link_scenes($scene_id);
+        }
+        if(!in_array($type, $this->defaultType)){
+            exit();
+        }
+        $this->render('/project/scene/'.$type, array('datas'=>$datas));
+    }
+
+    private function get_link_scenes($scene_id){
+        $scene_datas = array();
+        if(!$scene_id){
+            return $scene_datas;
+        }
+        //获取project_id
+        $scene_db = new Scene();
+        $scene_data = $scene_db->get_by_scene_id($scene_id);
+        if(!$scene_data || !$scene_data->project_id){
+            return $scene_datas;
+        }
+        $project_id = $scene_data->project_id;
+        $criteria=new CDbCriteria;
+        $criteria->order = 'id ASC';
+        $criteria->addCondition("project_id={$project_id}");
+        $criteria->addCondition("id!={$scene_id}");
+        $criteria->addCondition('status=1');
+        $scene_datas = $scene_db->findAll($criteria);
+        return $scene_datas;
+    }
+
+}
---
+++ protected/controllers/salado/modules/HotspotController.php
@@ -0,0 +1,30 @@
+<?php
+class HotspotController extends Controller{
+    public function actionSave(){
+        $request = Yii::app()->request;
+        $scene_id = $request->getParam('scene_id');
+        $datas['scene_id'] = $scene_id;
+        $this->check_scene_own($scene_id);
+        $datas['pan'] = (int)$request->getParam('pan');
+        $datas['tilt'] = (int)$request->getParam('tilt');
+        $datas['fov'] = (int)$request->getParam('fov');
+        $datas['type'] = $request->getParam('type');
+        $datas['link_scene_id'] = $request->getParam('link_scene_id');
+        $msg['flag'] = 0;
+        $msg['msg'] = '操作失败';
+        if($datas['link_scene_id'] == '0' || $datas['link_scene_id'] == ''){
+            $this->display_msg($msg);
+        }
+        $id = $this->add_hotspot($datas);
+        if(!$id){
+            $this->display_msg($msg);
+        }
+        $msg['flag'] = 1;
+        $msg['msg'] = '操作成功';
+        $this->display_msg($msg);
+    }
+    private function add_hotspot($datas){
+        $hotspot_db = new ScenesHotspot();
+        return $hotspot_db->add_hotsopt($datas);
+    }
+}
