5c377c2aae6b22a5f589650c0432d5844eb966c2
李文博 <wbli@anjuke.com>
2012-09-20 15:56:36 +0800

    a

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'Application',
+    'name'=>'test',
     'defaultController' => 'panos/list',
     'charset'=>'utf-8',
     // preloading 'log' component
