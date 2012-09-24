5f9fee9031a6aaf26582feb8ea855a0e10dfafdb
李文博 <wbli@anjuke.com>
2012-09-24 15:48:10 +0800

    a

--- pages/salado/scene.js
+++ pages/salado/scene.js
@@ -1,10 +1,10 @@
 function load_scene(box_id, scene_xml, player_url ){
-	var flashvars = {};
+    var flashvars = {};
     flashvars.xml = scene_xml;
     var params = {};
     params.menu = "false";
     params.quality = "high";
-    params.wmode = "Transparent";
+    params.wmode = "Window";
     params.allowfullscreen = "true";
     swfobject.embedSWF(player_url, box_id, "100%", "100%", "10.1.0", "", flashvars, params);
 }
