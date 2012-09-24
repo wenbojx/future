0135f2cf22340af51762e26e21c9755b73d9af9b
李文博 <wbli@anjuke.com>
2012-09-24 12:25:37 +0800

    a

--- html/test.php
+++ html/test.php
@@ -1,9 +1,8 @@
 <?php
 $md = new Memcached();
 $md->addServer('127.0.0.1', 11211);
-$v = $md->get('counter', null, $token);
-$md->cas($token, 'counter', $v + 1);
 
 $v = $md->set('aaa', 'asdfasdfasdf', '60000');
 $a = $md->get('aaa');
 echo $a;
+echo 111;
