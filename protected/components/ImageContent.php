8f125ebc5599956a2c18decb6d109b5cb76602ee
wenbojx <wenbojx@sina.com>
2012-09-26 20:32:12 +0800

    a

--- protected/components/ImageContent.php
+++ protected/components/ImageContent.php
@@ -56,7 +56,6 @@ class ImageContent {
      */
     public function get_img_info($datas, $size, $suffix='', $add_suffix = false){
         $pic_datas = array();
-        print_r($datas);
         if(!$datas){
             return false;
         }
@@ -71,12 +70,10 @@ class ImageContent {
         	$path = $path_original;
         }
         $path_new = $path.$size;
-        echo $path_new;
             if(!is_file($path_new)){
                 $explode_1 = explode('.', $size);
                 $explode_2 = explode('x', $explode_1[0]);
                 $path_original .= $datas['md5value'].'.'.$datas['type'];
-                echo $path_original;
                 if(!is_file($path_original)){
                 	return false;
                 }
