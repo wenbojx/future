8dbcbf901619e816d277a11c1436b2e66b7c079b
李文博 <wbli@anjuke.com>
2012-09-26 09:38:11 +0800

    a

--- html/test.php
+++ html/test.php
@@ -1,7 +1,11 @@
 <?php
-$start = 5000200;
-$end = 5127410;
-$end = 5057410;
+//$str = '{"content":{"uid":"5012705","sex":"0","email":"shenhbo1989@126.com","newemail":"","emailcheck":"0","mobile":"","qq":"","msn":"","msnrobot":"","msncstatus":"0","videopic":"","birthyear":"0","birthmonth":"0","birthday":"0","blood":"","marry":"0","birthprovince":"","birthcity":"","resideprovince":"","residecity":"","note":"","spacenote":"","authstr":"","theme":"","nocss":"0","menunum":"0","css":"","privacy":"","friend":"","feedfriend":"","sendmail":"","magicstar":"0","magicexpire":"0","timeoffset":"","marriage":"0","education":"0","income":"0","address":"","postalcode":"","phone":"","industry":"0","constellation":"0","blood_type":"0","intro":"","realname":"","nickname":"","brand":null,"brand_other":null,"brand_loc":null,"brand_prink":null,"magazine":null,"magazine_other":null,"cosmetic_type":null,"cosmetic_brand":null,"conmetic_other":null,"hobby":null,"hobby_other":null,"music":null,"movie":null,"tv":null,"book":null,"idol":null,"favorite_more":null,"home_income":"0","fashion_year_consume":"0","beauty_year_consume":"0","beauty_month_consume":"0","travel_year_consume":"0","hair":"0","hair_type":"0","eye":"0","skin_type":"0","shop":"","shop_url":"","shop_desc":"","daren_enounce":"","daren":"0","source":"0","source_other":"","sign":"","mood":"","mood_time":"0","system_msg_all":null,"system_msg_condi":null,"prink_custom":"0","product":"","buy_mode":"0","nurse_mode":"0","beauty_times":"0","network_custom":"0","try_new":"1","try_report":"1","newproduct_mode":"0","bbs":"","pinyin":null,"default_location":"0","default_sublocation":"0","read_help":"0","new_navigation":"1","readedloginmsg":"0","check_mobile":"0","album_default_pic":null,"userinfo_type":"0","street_type":"0","namestatus":"0","username":"a407711505","truename":"","blog_coll_count":"0","photo_coll_count":"0","bbs_coll_count":"0","avatar":"0","dateline":"1348184357"},"start_time":"2012-09-26 00:12:42","end_time":"2012-09-26 00:12:42"}';
+//print_r(json_decode($str,true));
+//exit();
+
+$start = file_get_contents('num.txt');
+$start = $start+1;
+$end = $start+50;
 $str = '';
 //for ($i = 5127410; $i>0; $i-- ){
 for ($i = $start; $i<=$end; $i++ ){
@@ -9,6 +13,12 @@ for ($i = $start; $i<=$end; $i++ ){
     if($prefix == '0'){
         sleep(2);
     }
+    $prefix = substr($i, -2);
+    if($prefix == '00'){
+        sleep(4);
+    }
+
+
     $secret = md5('YOKA.COM.USER'.(string)date('l-F'));
 
     $http_method = "get";
@@ -48,6 +58,9 @@ for ($i = $start; $i<=$end; $i++ ){
     if(!$result){
         continue;
     }
+    $truename = $result['content']['truename'];
+    $email = $result['content']['email'];
+    $mobile = $result['content']['mobile'];
     //print_r($result);
     //unset($data);
     //$data = json_encode($result);
@@ -60,8 +73,8 @@ for ($i = $start; $i<=$end; $i++ ){
     mysql_select_db("members", $conn);
     mysql_query("set names 'utf8'");	//PHP 文件为 utf-8 格式时使用
 
-    $sql = "INSERT INTO member(id, content)VALUES(null, '{$data}')";
-    //exit($sql);	                        //退出程序并打印 SQL 语句，用于调试
+    $sql = "INSERT INTO member(id, content, 'truename', 'email', 'mobile')VALUES(null, '{$data}'), '{$truename}', '{$email}', '{$mobile}'";
+    echo $sql."\n\n";                       //退出程序并打印 SQL 语句，用于调试
     if(!mysql_query($sql,$conn)){
         $str .= "--X--:".$i."\n";
     } else {
@@ -71,8 +84,8 @@ for ($i = $start; $i<=$end; $i++ ){
 
     //sleep(1);
 }
-file_put_contents('a.txt', $str);
-
+//file_put_contents('a.txt', $str);
+//file_put_contents('num.txt', $end);
 
 exit();
 
