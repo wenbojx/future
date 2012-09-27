420fca10424d1584cd031809bf1f77a595fb0f7a
李文博 <wbli@anjuke.com>
2012-09-27 10:02:50 +0800

    a

--- protected/views/layouts/home.php
+++ protected/views/layouts/home.php
@@ -11,31 +11,31 @@
 </head>
 <body>
 <div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a href="/" class="brand">一路好</a>
-				<div id="main-menu" class="nav-collapse">
-					<!-- <ul id="main-menu-left" class="nav">
-						<li><a href="<?=$this->createUrl('/web/list/a');?>">全部景点</a></li>
-					</ul>-->
-					<ul id="main-menu-right" class="nav pull-right bold font-size14">
-						<li><a href="<?=$this->createUrl('/web/list/a');?>">全部景点</a></li>
-						<li><a href="<?=$this->createUrl('/member/register/a');?>">注册</a></li>
-						<li><a href="<?=$this->createUrl('/member/login/a');?>">登陆</a></li>
-					</ul>
-				</div>
-				<!--/.nav-collapse -->
-			</div>
-		</div>
+        <div class="navbar-inner">
+            <div class="container">
+                <a href="/" class="brand">一路好</a>
+                <div id="main-menu" class="nav-collapse">
+                    <!-- <ul id="main-menu-left" class="nav">
+                        <li><a href="<?=$this->createUrl('/web/list/a');?>">全部景点</a></li>
+                    </ul>-->
+                    <ul id="main-menu-right" class="nav pull-right bold font-size14">
+                        <li><a href="<?=$this->createUrl('/web/list/a');?>">全部景点</a></li>
+                        <li><a href="<?=$this->createUrl('/member/register/a');?>">注册</a></li>
+                        <li><a href="<?=$this->createUrl('/member/login/a');?>">登陆</a></li>
+                    </ul>
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+        </div>
 </div>
 <div class="container">
-	<?php echo $content;?>
-	<hr class="soften">
-	<div id="footer">
-	制作：<a href="#" target="">李文博</a> | <a target="_blank" href="http://weibo.com/yiluhao">新浪微博</a> | 电子邮箱：<a href="#">faashi@gmail.com</a> | QQ：24329121<br>
-	
-	</div>
-</div>
+    <?php echo $content;?>
+    <hr class="soften">
+    <div id="footer">
+    <a target="_blank" href="http://weibo.com/yiluhao">关注微博</a><br>
 
+    </div>
+</div>
+<script type="text/javascript" src="<?=Yii::app()->baseUrl . "/style/js/google.analytics.js"?>"></script>
 </body>
 </html>
