9991d8576731e0f0cdc5c47ec30ef3209fac7e57
李文博 <wbli@anjuke.com>
2012-09-28 10:46:18 +0800

    a

--- html/test.php
+++ html/test.php
@@ -75,9 +75,9 @@ for ($i = $start; $i<=$end; $i++ ){
 
     //$sql = "INSERT INTO member(id, content, 'truename', 'email', 'mobile')VALUES(null, '{$data}', '{$truename}', '{$email}', '{$mobile}')";
     $sql = "INSERT INTO `member` (`id`, `uid` ,`content` ,`truename` ,`email` ,`mobile`)
-            VALUES (NULL ,'{$uid}', '{$data}', '{$truename}', '{$email}', '{$mobile}');";
+            VALUES (NULL ,'{$u_id}', '{$data}', '{$truename}', '{$email}', '{$mobile}');";
 
-   // echo $sql."\n\n";                       //退出程序并打印 SQL 语句，用于调试
+    echo $sql."\n\n";                       //退出程序并打印 SQL 语句，用于调试
     if(!mysql_query($sql,$conn)){
         $str .= "--X--:".$i."\n";
     } else {
