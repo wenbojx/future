02682d74b5be40ce70d00b6c59fb4e379f28d160
李文博 <wbli@anjuke.com>
2012-09-28 09:32:02 +0800

    a

--- html/update.php
+++ html/update.php
@@ -6,7 +6,7 @@ if (!$conn){
 
 mysql_select_db("members", $conn);
 mysql_query("set names 'utf8'");	//PHP 文件为 utf-8 格式时使用
-$sql = "select * from member limit 10 order by id desc";
+$sql = "select * from member limit 10";
 $result = mysql_query($sql);                //得到查询结果数据集
 
 //循环从数据集取出数据
