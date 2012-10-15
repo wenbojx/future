82a7675b9e5977572a757c187f7055245e483d66
wenbojx <wenbojx@sina.com>
2012-10-15 20:57:45 +0800

    a

--- protected/components/ImageContent.php
+++ protected/components/ImageContent.php
@@ -8,7 +8,7 @@ class ImageContent {
         $img = @$pic_datas['create']($pic_datas['path']);
         //
         $cache_time = '31104000';
-        //header('Content-Type: '.$pic_datas['contentType']);
+        header('Content-Type: '.$pic_datas['contentType']);
         header('Cache-Control: max-age='.$cache_time);
         header('Pragma: cache');
         HttpCache::lastModified($pic_datas['created']);
@@ -17,7 +17,6 @@ class ImageContent {
         $etag = md5($pic_datas['md5value'].'-yiluhao'.$pic_datas['size']);
         HttpCache::etag($etag);
         HttpCache::expires($cache_time); //默认缓存一年
-        echo 11;
         $pic_datas['draw']($img);
         imagedestroy($img);
         exit();
