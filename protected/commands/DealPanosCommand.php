16861b8dbdf42595d1a3c600c06241f30010aabf
wenbojx <wenbojx@sina.com>
2012-10-20 09:38:24 +0800

    a

--- protected/commands/DealPanosCommand.php
+++ protected/commands/DealPanosCommand.php
@@ -2,14 +2,14 @@
 class DealPanosCommand extends CConsoleCommand {
     public $defaultAction = 'index'; //默认动作
 
-    public $path = '/home/wbli/pics/0826';
+    public $path = 'C:\Users\faashi\Desktop\pics\0826';
     public $panos_path = array();
     public $default_new_folder = 'panos';
     public $default_pano_name = 'Panorama.jpg';
 
     public function actionFind() {
         $this->myscandir($this->path);
-        $new_folder = $this->path. '/' .$this->default_new_folder;
+        $new_folder = $this->path. DIRECTORY_SEPARATOR .$this->default_new_folder;
         if(!file_exists($new_folder)){
             mkdir($new_folder);
         }
@@ -17,16 +17,22 @@ class DealPanosCommand extends CConsoleCommand {
 
         //print_r($this->panos_path);
     }
+    public function actionSlipt(){
+    	$this->default_pano_name = 'Panorama-2.jpg';
+    	$path = '/mnt/hgfs/pics/panos/';
+    	$this->myscandir($path);
+    	print_r($this->panos_path);
+    }
     public function moveFiles(){
         if(!$this->panos_path){
             return false;
         }
         foreach($this->panos_path as $v){
-            $path_explode = explode('/', $v);
+            $path_explode = explode(DIRECTORY_SEPARATOR, $v);
             $num = count($path_explode)-3;
             $folder = $path_explode[$num];
-            $path = $this->path. '/' . $this->default_new_folder. '/'. $folder;
-            $new_file = $path.'/'.$this->default_pano_name;
+            $path = $this->path. DIRECTORY_SEPARATOR . $this->default_new_folder. DIRECTORY_SEPARATOR. $folder;
+            $new_file = $path.DIRECTORY_SEPARATOR.$this->default_pano_name;
 
             if(!file_exists($path)){
                 mkdir($path);
@@ -51,11 +57,11 @@ class DealPanosCommand extends CConsoleCommand {
 
         foreach(scandir($path) as $file){
             if($file!='.'  && $file!='..' && $file != $this->default_new_folder){
-                $path2= $path.'/'.$file;
+                $path2= $path.DIRECTORY_SEPARATOR.$file;
                 if(is_dir($path2)){
                     $this->myscandir($path2);
                 }else if($file == $this->default_pano_name){
-                    $this->panos_path[] = $path.'/'.$file;
+                    $this->panos_path[] = $path.DIRECTORY_SEPARATOR.$file;
                 }
             }
         }
--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,6 @@
 <?php
 
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/yiic.php';
+$yiic=dirname(__FILE__).'/../framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
-
+@putenv('YII_CONSOLE_COMMANDS='. dirname(__FILE__).'/commands' );
 require_once($yiic);
